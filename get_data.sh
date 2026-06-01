1930ba64833d81523e03c9720e06aa518a02e220
rena <renali@g.ucla.edu>
2026-05-31 18:06:24 -0700

    Add files via upload

---
+++ get_data.sh
@@ -0,0 +1,44 @@
+
+
+
+## getting the scRNA data
+
+mkdir expression
+mkdir genotypes
+cd ..
+cd expression
+mkdir -p h5ad
+cd h5ad
+
+wget -O onek1k.h5ad "https://datasets.cellxgene.cziscience.com/a3f5651f-cd1a-4d26-8165-74964b79b4f2.h5ad"
+
+
+### then run preprocessing, synthetic data generation, and simulating genotypes
+
+
+## generate gene annotation file
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_46/gencode.v46.annotation.gtf.gz
+
+zcat gencode.v46.annotation.gtf.gz | \
+grep -v '^#' | \
+awk '
+BEGIN{
+    OFS="\t";
+    print "gene_id","gene_name","chrom","tss"
+}
+$3=="gene"{
+    match($0,/gene_id "[^"]+"/)
+    gene_id=substr($0,RSTART+9,RLENGTH-10)
+
+    match($0,/gene_name "[^"]+"/)
+    gene_name=substr($0,RSTART+11,RLENGTH-12)
+
+    sub(/\..*/,"",gene_id)
+
+    tss=($7=="+")?$4:$5
+
+    chrom=$1
+    sub(/^chr/,"",chrom)
+
+    print gene_id,gene_name,chrom,tss
+}' > gene_annotation_nochr.tsv
